555bcfe45839d743ca158e9df3720b64d71472e7
paulm <pm308@students.uni-freiburg.de>
2026-04-20 18:44:12 +0200

    Added options force, no-warningd, cmp, no-write, no-bounds, force-real, force-time

--- src/cli/commands.py
+++ src/cli/commands.py
@@ -14,13 +14,21 @@ base_raw: dict[str, Optional[str] | Optional[list[str]]] = {
 }
 
 zummarize_specific_raw: dict[str, bool] = {
+    "force": False,
+    "no-warnings": False,
     "all": False,
     "sat": False,
     "unsat": False,
     "deep": False,
     "rank": False,
     "unsolved": False,
-    "solved": False
+    "solved": False,
+    "filter": False,
+    "cmp": False,
+    "no-write": False,
+    "no-bounds": False,
+    "force-real": False,
+    "force-time": False
 }
 
 
@@ -29,19 +37,29 @@ def base(zummarizepath: Annotated[Optional[str], typer.Option()] = None,
          logpaths: Annotated[Optional[list[str]], typer.Option()] = None,
          rlogpaths: Annotated[Optional[list[str]], typer.Option()] = None,
          configpath: Annotated[Optional[str], typer.Option()] = None,
+         force: bool = typer.Option(False, "--force", "-f"),
+         no_warnings: bool = typer.Option(False, "--no-warnings", "-n"),
          all: bool = typer.Option(False, "--all", "-a"),
          sat: bool = typer.Option(False, "--sat", "-s"),
          unsat: bool = typer.Option(False, "--unsat", "-u"),
          deep: bool = typer.Option(False, "--deep", "-d"),
          rank: bool = typer.Option(False, "--rank", "-r"),
          unsolved: bool = typer.Option(False, "--unsolved"),
-         solved: bool = typer.Option(False, "--solved")):
+         solved: bool = typer.Option(False, "--solved"),
+         filter: bool = typer.Option(False, "--filter"),
+         cmp: bool = typer.Option(False, "--cmp"),
+         no_write: bool = typer.Option(False, "--no-write"),
+         no_bounds: bool = typer.Option(False, "--no-bounds"),
+         force_real: bool = typer.Option(False, "--force-real"),
+         force_time: bool = typer.Option(False, "--force-time")):
 
     base_raw["zummarize_path"] = zummarizepath
     base_raw["log_paths"] = logpaths
     base_raw["r_log_paths"] = rlogpaths
     base_raw["config_path"] = configpath
 
+    zummarize_specific_raw["force"] = force
+    zummarize_specific_raw["no-warnings"] = no_warnings
     zummarize_specific_raw["all"] = all
     zummarize_specific_raw["sat"] = sat
     zummarize_specific_raw["unsat"] = unsat
@@ -49,6 +67,12 @@ def base(zummarizepath: Annotated[Optional[str], typer.Option()] = None,
     zummarize_specific_raw["rank"] = rank
     zummarize_specific_raw["unsolved"] = unsolved
     zummarize_specific_raw["solved"] = solved
+    zummarize_specific_raw["filter"] = filter
+    zummarize_specific_raw["cmp"] = cmp
+    zummarize_specific_raw["no-write"] = no_write
+    zummarize_specific_raw["no-bounds"] = no_bounds
+    zummarize_specific_raw["force-real"] = force_real
+    zummarize_specific_raw["force-time"] = force_time
 
 
 @app.command()
