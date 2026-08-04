3f343636a394d984816526671dc7c8610912de92
paulm <pm308@students.uni-freiburg.de>
2026-08-04 12:01:56 +0200

    Added documentation in load_data.py

--- src/core/load_data.py
+++ src/core/load_data.py
@@ -2,6 +2,16 @@ import pandas as pd
 
 
 def read_zummary(cfg) -> dict[str, pd.DataFrame]:
+    """
+    Loads all zummarys in pandas dataframes.
+
+    Parameters:
+    cfg: The configuration.
+
+    Returns:
+    dict[str, pd.DataFrame]: A dictionary of all zummarys that are loaded in pandas DataFrames.
+    The key of the dictionary is the name of the folder that contains the zummary.
+    """
     res = {}
     for log_path in cfg.log_paths:
         zummary_path = log_path / "zummary"
