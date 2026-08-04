16541b54879b32aa79ae7e43ded184adb78decfe
paulm <pm308@students.uni-freiburg.de>
2026-08-04 11:46:36 +0200

    Added documentation in save_data.py

--- src/core/save_data.py
+++ src/core/save_data.py
@@ -2,9 +2,16 @@ import yaml
 
 
 def write_dict_to_yaml(data: dict, filename):
+    """
+    Creates a new yaml file or overwrites an existing one and saves the configuration cfg in that file.
+
+    Parameters:
+    data (dict): The data that should be saved (only used for cfg or solver_style).
+    filename: The path to the savefile.
+    """
     try:
         with open(filename, "w", encoding="utf-8") as outfile:
-            # default_flow_style=False ensures readable block-style YAML [InlineCitation-1-python - How can I write data in YAML format in a file? - Stack Overflow](https://stackoverflow.com/questions/12470665/how-can-i-write-data-in-yaml-format-in-a-file)
+            # default_flow_style=False ensures readable block-style YAML
             yaml.dump(data, outfile, default_flow_style=False)
         print(f"YAML successfully written to {filename}")
     except OSError as e:
@@ -12,11 +19,23 @@ def write_dict_to_yaml(data: dict, filename):
 
 
 def save_config(cfg):
+    """
+    Saves the configuration cfg in a yaml file.
+
+    Parameters:
+    cfg: The configuration.
+    """
     if cfg.save_config is not None:
         write_dict_to_yaml(cfg.atr, cfg.save_config)
 
 
 def save_solver_style(cfg):
+    """
+    Only saves the solver_style. The rest of the config file remains unchanged.
+
+    Paramters:
+    cfg: The configuration.
+    """
     if cfg.save_config is not None:
         with open(cfg.save_config, "r") as file:
             data = yaml.safe_load(file)
