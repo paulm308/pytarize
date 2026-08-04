a4f49573d888a5bfbbd1c4b24c89196f01c03e0d
paulm <pm308@students.uni-freiburg.de>
2026-08-04 11:33:33 +0200

    Added documentation in transform.py

--- src/core/transform.py
+++ src/core/transform.py
@@ -3,6 +3,19 @@ from src.core.configuration_data import PlotType
 
 
 def transform(data: dict[str, pd.DataFrame], cfg):
+    """
+    Performs transformations that are relevant for all plot types. This includes
+    --sat, --unsat and --time.
+
+    Paramters:
+    data (dict[str, pd.DataFrame]): A dictionary of all zummarys that are loaded in pandas DataFrames.
+    The key of the dictionary is the name of the folder that contains the zummary.
+    cfg: The configuration.
+
+    Returns:
+    dict[str, pd.DataFrame]: The transformed data.
+    """
+    # handle sat and unsat
     if "--sat" in cfg.zummarize_cli and "--usat" in cfg.zummarize_cli:
         print("error: '--sat-only' and '--unsat-only'")
         return
@@ -14,6 +27,7 @@ def transform(data: dict[str, pd.DataFrame], cfg):
             for folder_name in data.keys():
                 data[folder_name] = data[folder_name][data[folder_name]["result"] == 20]
 
+    # handle time and real
     if "time" in cfg.global_atr.keys() and cfg.global_atr["time"] and "real" in cfg.global_atr.keys() and cfg.global_atr["real"]:
         print("error: '--real-only' and '--time-only'")
         return
