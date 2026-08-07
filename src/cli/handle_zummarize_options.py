16b485517ea7dc4df1f20cf8da7f926dcd1d65d8
paulm <pm308@students.uni-freiburg.de>
2026-08-07 12:58:16 +0200

    Added documentation in handle_zummarize_options.py

--- src/cli/handle_zummarize_options.py
+++ src/cli/handle_zummarize_options.py
@@ -1,5 +1,14 @@
 
 def create_zummarize_options(opts):
+    """
+    Creates a option string --option for all options in opts.
+
+    Parameters:
+    opts: The zummarize specific options.
+
+    Returns:
+    list[str]: The option strings.
+    """
     zummarize_cli = []
     for opt in opts.keys():
         if opt == "verbose" and opts[opt] is not None:
